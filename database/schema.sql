c2d17855baefa9d6653564be1aa8a101f30c9924
Antipascal <pesochin_evgeniy@mail.ru>
2024-07-20 14:37:53 +0300

    refactor sql scheme

--- database/schema.sql
+++ database/schema.sql
@@ -1,60 +1,85 @@
 CREATE SCHEMA urent;
 
-create table urent.house (
-    id varchar(255) primary key,
-    title varchar(255),
-    photos varchar(255) /*how??*/,
-    price integer,
-    max_capacity integer,
-    location float[],
-    availability boolean,
-    calendar varchar /*how??*/
+CREATE TABLE urent.house (
+    id text NOT NULL,
+    title text NOT NULL,
+    price integer NOT NULL,
+    max_capacity integer NOT NULL,
+    address text,
+    availability boolean NOT NULL DEFAULT TRUE,
+--     photos varchar(255) /*how??*/,
+--     calendar varchar /*how??*/
+
+    CONSTRAINT pk_house_id PRIMARY KEY (id),
+    CONSTRAINT check_price CHECK (price > 0)
+    CONSTRAINT check_max_capacity CHECK (max_capacity > 0)
 );
 
-create table urent.booking (
-    id varchar(255) primary key,
-    foreign key (id) references urent.house(id),
-    dates date[]
+CREATE TABLE urent.booking (
+    id text NOT NULL,
+    check_in date,
+    check_out date,
+
+    CONSTRAINT pk_booking_id PRIMARY KEY (id),
+    CONSTRAINT fk_house FOREIGN KEY (id)
+        REFERENCES urent.house
 );
 
-create table urent.owner (
-    id varchar(255) primary key,
-    name varchar(255),
-    booking urent.booking[],
-    telephone_number text,
-    telegram text
+CREATE TABLE urent.owner (
+    id text NOT NULL,
+    name text NOT NULL,
+--     booking urent.booking[],
+    phone_number text,
+    telegram text,
+
+    CONSTRAINT pk_owner_id PRIMARY KEY (id)
 );
 
-create table urent.tenant_form (
-    id varchar(255) primary key,
-    name varchar(255),
-    telephone_number text,
-    communication_method text,
+CREATE TYPE urent.communication_method AS ENUM ('PHONE', 'TELEGRAM', 'WHATSAPP');
+
+CREATE TABLE urent.tenant_form (
+    id text NOT NULL,
+    name text NOT NULL,
+    phone_number text,
+    communication_method urent.communication_method,
     guests_number integer,
-    dates date[],
+    check_in date,
+    check_out date,
     comment text,
-    foreign key (id) references house(id)
+
+    CONSTRAINT pk_tenant_form_id PRIMARY KEY (id),
+    CONSTRAINT fk_house FOREIGN KEY (id)
+        REFERENCES urent.house
 );
 
-create type urent.bathroom as enum ('bath', 'shower');
+CREATE TYPE urent.bathroom_type AS ENUM ('BATH', 'SHOWER');
 
-create table urent.description (
-    id varchar(255) primary key,
-    foreign key (id) references urent.house(id),
-    bathroom urent.bathroom,
+CREATE TABLE urent.description (
+    id text NOT NULL,
+    bathroom_type urent.bathroom_type,
     towels boolean,
     linen boolean,
-    furniture varchar[],
+    furniture text[],
     barbecue_area boolean,
     garage boolean,
     house_area float,
     room_amount integer,
     floor_amount integer,
-    appliances varchar[],
-    utilities varchar[]
+    appliances text[],
+    utilities text[],
+
+    CONSTRAINT pk_description_id PRIMARY KEY (id),
+    CONSTRAINT fk_house FOREIGN KEY (id)
+        REFERENCES urent.house
 );
 
-create table urent.owner_house (
-    house_id varchar(255) primary key,
-    owner_id varchar(255) 
+CREATE TABLE urent.owner_house (
+    house_id text NOT NULL,
+    owner_id text NOT NULL,
+
+    CONSTRAINT pk_owner_house PRIMARY KEY (house_id),
+    CONSTRAINT fk_house FOREIGN KEY (house_id)
+        REFERENCES urent.house,
+    CONSTRAINT fk_owner FOREIGN KEY (owner_id)
+        REFERENCES urent.owner
 )
